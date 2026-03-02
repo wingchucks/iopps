52fb00dff9ccfa12361f21ba34324b5364a26f5b
Nathan Arias <nathan.arias@iopps.ca>
2026-03-02 13:49:14 -0600

    fix: X-Frame-Options DENY -> SAMEORIGIN, fix sitemap URL in robots.txt

--- next.config.ts
+++ next.config.ts
@@ -44,7 +44,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "X-Frame-Options",
-            value: "DENY",
+            value: "SAMEORIGIN",
           },
           {
             key: "Referrer-Policy",
